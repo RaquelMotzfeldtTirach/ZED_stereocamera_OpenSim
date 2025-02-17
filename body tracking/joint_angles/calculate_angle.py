42474d5cdc4620671f9059866988f59231aa47ee
RaquelMotzfeldtTirach <93975644+RaquelMotzfeldtTirach@users.noreply.github.com>
2025-02-17 22:45:12 +0100

    wrist joint and deleted dot product

--- body tracking/joint_angles/calculate_angle.py
+++ body tracking/joint_angles/calculate_angle.py
@@ -3,7 +3,8 @@ import matplotlib.pyplot as plt
 
 # Notes to myself
 # dot product or cross product gives the same angles :)
-# 
+# todo: colors depending on average confidence of the joints
+# todo: rethink max and min values for the plot, some joints are not in the range 0-180
 
 class JointAnglesPlotter:
     def __init__(self):
@@ -13,24 +14,14 @@ class JointAnglesPlotter:
         self.ax.set_xlabel('Joints and Methods')
         self.ax.set_ylabel('Angles (degrees)')
         self.ax.set_title('Joint Angles by Different Methods')
-        self.angle_methods = ['dot_product', 'cross_product']
-        self.joints = ['left_elbow', 'right_elbow', 'neck']
+        self.angle_methods = ['cross_product']
+        self.joints = ['left_elbow', 'right_elbow', 'neck', 'left_wrist', 'right_wrist', 'left_shoulder', 'right_shoulder', 'pelvis', 'spine_1', 'spine_2', 'spine_3']
         self.bar_positions = range(len(self.joints) * len(self.angle_methods))
         self.bars = self.ax.bar(self.bar_positions, [0] * len(self.bar_positions), color='blue')
         self.ax.set_xticks(self.bar_positions)
         self.ax.set_xticklabels([f'{joint}_{method}' for joint in self.joints for method in self.angle_methods], rotation=45)
         plt.show()
 
-    def calculate_angle_dot_product(self, p1, p2, p3):
-        '''
-        Calculate the angle between three points using dot product.
-        The points are expected to be numpy arrays with 3 elements (x, y, z).
-        '''
-        v1 = p1 - p2
-        v2 = p3 - p2
-        angle_rad = np.arccos(np.dot(v1, v2) / (np.linalg.norm(v1) * np.linalg.norm(v2)))
-        angle_deg = np.degrees(angle_rad)
-        return angle_deg
 
     def calculate_angle_cross_product(self, p1, p2, p3):
         '''
@@ -44,6 +35,7 @@ class JointAnglesPlotter:
         angle_rad = np.arctan2(cross_prod_len, dot_prod)
         angle_deg = np.degrees(angle_rad)
         return angle_deg
+    
 
     def get_joint_angles(self, body):
         '''
@@ -51,9 +43,9 @@ class JointAnglesPlotter:
         This only works for a 38-joint body format.
         '''
         angles = {method: {} for method in self.angle_methods}
+
         
         def calculate_angles(p1, p2, p3, angles, joint_name):
-            angles['dot_product'][joint_name] = self.calculate_angle_dot_product(p1, p2, p3)
             angles['cross_product'][joint_name] = self.calculate_angle_cross_product(p1, p2, p3)
         
         # Elbow angles (between shoulder, elbow, and wrist)
@@ -61,6 +53,7 @@ class JointAnglesPlotter:
             body.keypoint[14] is not None and
             body.keypoint[16] is not None):
             calculate_angles(body.keypoint[12], body.keypoint[14], body.keypoint[16], angles, 'left_elbow')
+            print('elbow confidence:', body.keypoint_confidence[14])
         
         if (body.keypoint[13] is not None and 
             body.keypoint[15] is not None and
@@ -74,17 +67,6 @@ class JointAnglesPlotter:
             body.keypoint[7] is not None):
             ear_midpoint = (body.keypoint[6] + body.keypoint[7]) / 2
             calculate_angles(ear_midpoint, body.keypoint[4], body.keypoint[3], angles, 'neck')
-
-        # Wrist angles (between elbow, wrist, and hand) TODO !
-        if (body.keypoint[14] is not None and 
-            body.keypoint[16] is not None and
-            body.keypoint[18] is not None):
-            calculate_angles(body.keypoint[14], body.keypoint[16], body.keypoint[18], angles, 'left_wrist')
-        
-        if (body.keypoint[15] is not None and 
-            body.keypoint[17] is not None and
-            body.keypoint[19] is not None):
-            calculate_angles(body.keypoint[15], body.keypoint[17], body.keypoint[19], angles, 'right_wrist')
 
         # Shoulder angles (between clavicle, shoulder, and elbow) 
         if (body.keypoint[14] is not None and 
@@ -122,8 +104,26 @@ class JointAnglesPlotter:
             body.keypoint[3] is not None and
             body.keypoint[4] is not None):
             calculate_angles(body.keypoint[2], body.keypoint[3], body.keypoint[4], angles, 'spine_3')
+
+
+        # Calculate left wrist angle
+        if (body.keypoint[14] is not None and 
+            body.keypoint[16] is not None and
+            body.keypoint[32] is not None and
+            body.keypoint[36] is not None):
+            fingers_midpoint_left = (body.keypoint[32] + body.keypoint[36]) / 2
+            calculate_angles(body.keypoint[14], body.keypoint[16], fingers_midpoint_left, angles, 'left_wrist')
+
+        # Calculate right wrist angle
+        if (body.keypoint[15] is not None and 
+            body.keypoint[17] is not None and
+            body.keypoint[33] is not None and
+            body.keypoint[37] is not None):
+            fingers_midpoint_right = (body.keypoint[33] + body.keypoint[37]) / 2
+            calculate_angles(body.keypoint[15], body.keypoint[17], fingers_midpoint_right, angles, 'right_wrist')
         
         return angles
+       
     
     def update_plot(self, angles):
         '''
