33efa670c822a25354f083e0b4685af7c360ea5d
RaquelMotzfeldtTirach <93975644+RaquelMotzfeldtTirach@users.noreply.github.com>
2025-09-05 16:42:06 +0200

    drop row if nan

--- body tracking/post_processing/convert_csv_to_formated_csv.py
+++ body tracking/post_processing/convert_csv_to_formated_csv.py
@@ -56,6 +56,8 @@ def transform_csv(file_path):
         col.replace('RIGHT_', 'TEMP_').replace('LEFT_', 'RIGHT_').replace('TEMP_', 'LEFT_') if 'RIGHT_' in col or 'LEFT_' in col else col
         for col in pivot_df.columns
     ]
+    # Delete the whole row if any of the columns are NaN
+    pivot_df.dropna(how='any', inplace=True)
 
     # Create the output filename
     base_name = os.path.basename(file_path)
