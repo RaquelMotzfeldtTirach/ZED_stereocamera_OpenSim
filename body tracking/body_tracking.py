b972e978812b7bb929e9b46ee22b2db7edd7726f
RaquelMotzfeldtTirach <93975644+RaquelMotzfeldtTirach@users.noreply.github.com>
2025-11-11 13:40:18 +0100

    more flexible start

--- body tracking/body_tracking.py
+++ body tracking/body_tracking.py
@@ -96,7 +96,7 @@ def terminal_listener(stop_flag):
             stop_flag.set()
             break
 
-def main():
+def main(ID, TRIAL):
     print("Running Body Tracking sample ... Press 'q' to quit")
 
     # Create a Camera object
@@ -155,8 +155,15 @@ def main():
     key_wait = 1 
 
     # Create a folder and a CSV file to save the keypoints
-    id = input("Enter the subject ID: ")
-    mvt_id = input("Enter the movement description: ")
+    if ID != "":
+        id = ID
+    else:
+        id = input("Enter the subject ID: ")
+    if TRIAL != "":
+        mvt_id = TRIAL
+    else: 
+        mvt_id = input("Enter the trial description: ")
+
     file_name = 'recordings/subject'+ id +'/stereocamera_'+ mvt_id +'.csv'
     # Create subject## folder
     try:
@@ -253,8 +260,10 @@ if __name__ == '__main__':
     parser.add_argument('--input_svo_file', type=str, help='Path to an .svo file, if you want to replay it',default = '')
     parser.add_argument('--ip_address', type=str, help='IP Adress, in format a.b.c.d:port or a.b.c.d, if you have a streaming setup', default = '')
     parser.add_argument('--resolution', type=str, help='Resolution, can be either HD2K, HD1200, HD1080, HD720, SVGA or VGA', default = '')
+    parser.add_argument('--ID', type=str, help='Subject ID', default = '')
+    parser.add_argument('--TRIAL', type=str, help='Trial ID', default = '')
     opt = parser.parse_args()
     if len(opt.input_svo_file)>0 and len(opt.ip_address)>0:
         print("Specify only input_svo_file or ip_address, or none to use wired camera, not both. Exit program")
         exit()
-    main() 
+    main(opt.ID, opt.TRIAL) 
